32521c1db98b2a4e0e6cbbe049c1196ef9ef78db
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2019-09-24 00:14:15 -0400

    adds routes config

---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+
+import Details from '../pages/Details';
+import New from '../pages/New';
+import Edit from '../pages/Edit';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
+
+      <Route path="/meetup" component={Details} />
+      <Route path="/new" component={New} />
+      <Route path="/edit" component={Edit} />
+    </Switch>
+  );
+}
