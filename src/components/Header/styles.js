410c27e3269239d43255f9dd56b8e26d2ce4b734
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2019-09-27 23:38:25 -0400

    Revert "Revert "adds dashboard styling""
    This reverts commit a9f00e17555d2fa72e8a1d8078bf5f42d2c24a01.

---
+++ src/components/Header/styles.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: rgba(0, 0, 0, 0.3);
+  padding: 0 30px;
+`;
+
+export const Content = styled.div`
+  height: 64px;
+  max-width: 940px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  nav {
+    display: flex;
+    align-items: center;
+
+    img {
+      margin-right: 20px;
+      padding-right: 20px;
+      border-right: 1px solid #333;
+    }
+
+    a {
+      font-weight: bold;
+      color: #f94d6a;
+    }
+  }
+
+  aside {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  margin-left: 20px;
+  padding-left: 20px;
+  border-left: 1px solid #333;
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #fff;
+    }
+
+    a {
+      display: block;
+      margin-top: 2px;
+      font-size: 12px;
+      color: #999;
+    }
+  }
+
+  img {
+    height: 32px;
+    border-radius: 50%;
+  }
+`;
