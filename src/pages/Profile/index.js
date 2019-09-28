af8fb013d245c91709d8fa23c46fef59e8c7b598
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2019-09-28 14:46:03 -0400

    adds profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import { FaPlusCircle, FaPowerOff } from 'react-icons/fa';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">
+          <FaPlusCircle />
+          Salvar perfil
+        </button>
+      </Form>
+
+      <button type="button">
+        <FaPowerOff />
+        Sair do Meetapp
+      </button>
+    </Container>
+  );
 }
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,89 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 940px;
+  margin: 50px auto;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: rgba(0, 0, 0, 0.1);
+      border: 0;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #fff;
+      margin: 0 0 10px;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+
+    span {
+      color: #f64c75;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+    }
+
+    hr {
+      border: 0;
+      height: 1px;
+      background: rgba(255, 255, 255, 0.2);
+      margin: 10px 0 20px;
+    }
+
+    button {
+      display: flex;
+      align-self: flex-end;
+      padding: 0 15px;
+      margin: 5px 0 0;
+      height: 44px;
+      background: #f94d6a;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+      align-items: center;
+
+      &:hover {
+        background: ${darken(0.03, '#f94d6a')};
+      }
+
+      svg {
+        margin-right: 5px;
+      }
+    }
+  }
+
+  > button {
+    display: flex;
+    align-items: center;
+    float: right;
+    padding: 0 15px;
+    margin: 10px 0 0;
+    height: 44px;
+    background: #ec592b;
+    font-weight: bold;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.08, '#ec592b')};
+    }
+
+    svg {
+      margin-right: 5px;
+    }
+  }
+`;
