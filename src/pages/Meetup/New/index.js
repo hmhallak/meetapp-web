7f18c9c1441e926a3922f7757d409f9e5c9d0131
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2019-10-08 23:30:34 -0400

    adds meetup creation page

--- src/pages/Meetup/New/index.js
+++ src/pages/Meetup/New/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { FaPlusCircle, FaCamera } from 'react-icons/fa';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+import BannerInput from '~/pages/Meetup/BannerInput';
+
+import { Container, Image } from './styles';
 
 export default function New() {
-  return <div />;
+  async function handleSubmit({ title, description, date, location }) {
+    const response = await api.post('meetups', {
+      title,
+      description,
+      location,
+      date,
+    });
+    console.tron.log(response);
+  }
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Image>
+          <FaCamera size={54} />
+          <strong>Selecionar imagem</strong>
+        </Image>
+        <Input name="title" placeholder="Título do Meetup" />
+        <Input multiline name="description" placeholder="Descrição completa" />
+
+        <Input name="date" placeholder="Data do meetup" />
+        <Input name="location" placeholder="Localização" />
+
+        <button type="submit">
+          <FaPlusCircle />
+          Salvar meetup
+        </button>
+      </Form>
+    </Container>
+  );
 }
